9545381dcc8739d294c30396981b6ffee25134a0
Mutoxicated <96009711+Mutoxicated@users.noreply.github.com>
2022-03-01 22:49:17 +0200

    Delete Ships/Mutoxicated-Guardian directory

--- Ships/Mutoxicated-Guardian/Guardian.lua
+++
@@ -1,7 +0,0 @@
-meshes = {
-    {
-        vertexes = {{-17,0},{-11,-6},{-5,0},{-11,6},  {-13,-16},{1,-3},{16,-3},{3,-16},  {-13,16},{1,3},{16,3},{3,16}},
-        colors = {0x30ffffff, 0x30ffffff, 0x30ffffff, 0x30ffffff,  0x30ffffff, 0xffff30ff, 0xffff30ff, 0xffff30ff,   0x30ffffff, 0xffff30ff, 0xffff30ff, 0xffff30ff},
-        segments = {{0,1,2,3,0},{4,5,6,7,4},{8,9,10,11,8}}
-    }
-}
