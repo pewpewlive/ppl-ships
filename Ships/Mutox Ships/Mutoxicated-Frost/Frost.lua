710922cdf47b18ccb53c08bab4d94cbcf36911f7
Mutoxicated <96009711+Mutoxicated@users.noreply.github.com>
2022-03-03 16:24:40 +0200

    Add files via upload

---
+++ Ships/Mutox Ships/Mutoxicated-Frost/Frost.lua
@@ -0,0 +1,7 @@
+meshes = {
+    {
+        vertexes = {{-16,-3},{-7,-14},{-2,-14}, {4,-7},{-2,-7}, {-8,0}, {-2,7},{4,7}, {-2,14},{-7,14},{-16,3},  {4,-14},{10,-14},{16,-7},{10,-7}, {4,14},{10,14},{16,7},{10,7},  {8,-2},{0,-2},{-2,0},{0,2},{8,2},{10,0}},
+        colors = {0x454545ff, 0x454545ff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x454545ff, 0x454545ff,    0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff,   0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff,    0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff},
+        segments = {{0,1,2,3,4,5,6,7,8,9,10,0}, {11,12,13,14,11}, {15,16,17,18,15}, {19,20,21,22,23,24,19}},
+    }
+}
