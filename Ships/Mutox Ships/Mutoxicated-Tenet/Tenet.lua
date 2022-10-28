b13836eb0e4ff36ce4520671ab8089eaf542af93
Mutoxicated <96009711+Mutoxicated@users.noreply.github.com>
2022-10-29 02:39:48 +0300

    Add files via upload

---
+++ Ships/Mutox Ships/Mutoxicated-Tenet/Tenet.lua
@@ -0,0 +1,17 @@
+meshes = {
+    {--tenet
+    vertexes = {{-4,-4/2-1},{-4/2,-4-1},{4/2,-4-1},{4,-4/2-1},{4,4/2-1},{4/2,4-1},{-4/2,4-1},{-4,4/2-1},--the circle
+                {3.5,-8},{3.5,-12.5},{10,-4.5},{15.5,-4},{3.5,14},{3.5,6},
+                {-3.5,-8},{-3.5,-12.5},{-10,-4.5},{-15.5,-4},{-3.5,14},{-3.5,6},
+                },
+    segments = {{0,1,2,3,4,5,6,7,0},
+                {8,9,10,11,12,13},
+                {14,15,16,17,18,19}
+                },
+    colors = {
+        0xffffee90,0xffffff60,0xffffff60,0xffffee90,0xffffdddd,0xffffccff,0xffffccff,0xffffdddd,
+        0xffffaa80,0xffffff50,0xffffbb9a,0xffffddac,0xffffddde,0xffffffff,
+        0xffffaa80,0xffffff50,0xffffbb9a,0xffffddac,0xffffddde,0xffffffff,
+    }
+    }
+}
