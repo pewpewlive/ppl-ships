cddee9c9569e7a52ace4f5da2173ccc8dec37c00
Mutoxicated <96009711+Mutoxicated@users.noreply.github.com>
2023-05-04 22:39:32 +0300

    Add files via upload

---
+++ Ships/Mutox Ships/Mutoxicated-Origami/Origami.lua
@@ -0,0 +1,21 @@
+meshes = {
+    {
+        vertexes = {{0,0},{-10,14},{-3,11.8}, {-1,11.2},{1,10.5}, {3,10},{5,9.2}, {7,8.8},{14,6.5},{1,0},{14,-6.5},{7,-8.8}, {3,-10},{5,-9.2}, {1,-10.5},{-1,-11.2}, {-3,-11.8},{-10,-14},--main body
+        {-11,15},{-13,6},{-5,8},
+        {-11,-15},{-13,-6},{-5,-8}
+        },
+        colors = {
+        0xdaffedff,0xdaffedff,0xdaffedff,
+        0x37bdffff,0x37bdffff,
+        0x37bdffff,0x37bdffff,
+        0xdaffedff,0xdaffedff,0xdaffedff,0xdaffedff,0xdaffedff,
+        0x37bdffff,0x37bdffff,
+        0x37bdffff,0x37bdffff,
+        0xdaffedff,0xdaffedff,
+        
+        0x37aaffff,--anomaly
+        0x37aaffff,0x37aaffff,0x37aaffff,
+        0x37aaffff,0x37aaffff,0x37aaffff},
+        segments = {{0,1,2},{3,4},{5,6},{7,8,9,10,11},{12,13},{14,15},{16,17,0}, {18,19,20},{21,22,23}}
+    }
+}
